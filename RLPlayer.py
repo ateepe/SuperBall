053fd9ff3e1faaafb64a2d830651a87dcbe6c62a
DESKTOP-HAF932\ateepe <ateepe@vols.utk.edu>
2018-11-13 13:41:26 -0500

    Add questions/documentation for the design

--- RLPlayer.py
+++ RLPlayer.py
@@ -6,6 +6,15 @@ class RLPlayer:
     def __init__(self, nRows, nCols, goals, colors, minSetSize):
         self.game = SuperBall.SuperBall(nRows, nCols, goals, colors, minSetSize);
 
+        # actions = pick any two tiles and swap them, or any of the goal tiles and try to score
+        #           if any action can't be taken, just allow the machine to take it but nothing happens and there is no reward
+        # states = the state of the board
+        # 
+        # since actions of swap and score are different dimenstiosn (two tiles for swap vs one tile for score), should policy and action
+        # value data structures be dictionaries to accommodate?
+        # 
+        # Since so many possible states, should they be added dynamically as encountered rather than initialize all at once? 
+
     def sarsa(self, num_episdoes):
         pass
 
