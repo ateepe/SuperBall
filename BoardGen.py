8c3e9b122709486f0a0144207d484d76eae70e01
Samuel Brown <sbrow109@vols.utk.edu>
2018-11-24 01:50:24 -0500

    Added a function to BoardGen.py to generate higher scoring boards.

--- BoardGen.py
+++ BoardGen.py
@@ -1,3 +1,4 @@
+import DisjointSet as dj
 import BoardScorer as bs
 import numpy as np
 
@@ -10,6 +11,67 @@ def gen_random_data():
 		tile_index = np.random.randint(len(tiles))
 		new_data[i] = tiles[tile_index]
 	return new_data
+
+def gen_large_set_data(max_set_size=5, rows=8, cols=10):
+	"""Generate boards with larger tile sets that will score higher.
+
+	Arguments:
+		max_set_size (int): The maximum size of neighboring disjoint sets. If
+		                    the max size changes randomly (and favors lower
+		                    set sizes), the board will be more realistic.
+		rows (int): The number of rows on the SuperBall board.
+		cols (int): The number of cols on the SuperBall board.
+
+	Returns:
+		np.chararray: An array representing the different tile values.
+	"""
+
+	board = np.chararray((80), unicode=True)
+	djSet = dj.DisjointSet(rows*cols)
+
+	# Determine possible places for unions of sets randomly
+	set_divisions = set()
+	for r in range(rows-1):
+		for c in range(cols):
+			cell_index = r*cols + c
+			coin_flip = np.random.binomial(1, 0.5, 1)[0]
+			if (coin_flip == 1):
+				set_divisions.add(cell_index)
+	for r in range(rows):
+		for c in range(cols-1):
+			cell_index = (r*cols + c) + (rows*cols)
+			coin_flip = np.random.binomial(1, 0.5, 1)[0]
+			if (coin_flip == 1):
+				set_divisions.add(cell_index)
+
+	# Union the sets if both neighbors are less than max_set_size
+	# TODO --> Change max_set_size randomly to get more realistic boards
+	# TODO --> Probably favor lower set sizes with max_set_size
+	for s in set_divisions:
+		if (s < rows*cols):
+			size1 = djSet.getSetSize(s)
+			size2 = djSet.getSetSize(s + cols)
+			if (size1 < max_set_size and size2 < max_set_size):
+				djSet.union(s, s+cols)
+		else:
+			size1 = djSet.getSetSize(s - rows*cols)
+			size2 = djSet.getSetSize(s - rows*cols + 1)
+			if (size1 < max_set_size and size2 < max_set_size):
+				djSet.union(s - rows*cols, s - rows*cols + 1)
+
+	# Choose a color for each disjoint set and fill in board
+	set_colors = {}
+	for r in range(rows):
+		for c in range(cols):
+			index = r*cols + c
+			setID = djSet.getSetID(index)
+			if setID not in set_colors:
+				tile_index = np.random.randint(len(tiles))
+				set_colors[setID] = tiles[tile_index]
+			board[index] = set_colors[setID]
+
+	return board
+
 
 def print_data(data):
 	for r in range(8):
@@ -77,6 +139,15 @@ def load_data(path='datasets/randomBoards.npz'):
 		x_test, y_test = f['x_test'], f['y_test']
 	return (x_train, y_train), (x_test, y_test)
 
+
+# Code to test the larger sets board generator
+# TODO --> Question: does this generator behave like the completely random one
+#          when the max_set_size is set to 1?
+board = gen_large_set_data()
+score, score_pos = bs.analyze_board(board)
+print_data(board)
+print(score)
+exit()
 
 # Test the board generation/save/load
 save_npz(num_training_samples=2, num_testing_samples=2)
